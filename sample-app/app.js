384bd53714433f6ba43366b1ebbd9b941950658c
HardikYadav99 <hardikyadav1567@gmail.com>
2026-06-10 07:03:29 +0530

    day 4: add sample app, dockerfile, and github actions workflow

---
+++ sample-app/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain' });
+    res.end('DevPortal sample app is running! \n');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
